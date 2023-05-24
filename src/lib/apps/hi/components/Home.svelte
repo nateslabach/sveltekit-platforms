4b7762f15e72975caee13c1c556d22dc30fb7ff5
nateslabach <134459912+nateslabach@users.noreply.github.com>
2023-05-24 05:00:20 +0000

    squares

--- src/lib/apps/hi/components/Home.svelte
+++ src/lib/apps/hi/components/Home.svelte
@@ -6,13 +6,14 @@
   justify-content: space-around;
   align-items: center;
   height: 80vh; /* Adjust this value to change the height of the hero section */
-  background-color: #f0f0f0; /* Change this value to the desired background color */
+  background-color: ##0997fd
+; /* Change this value to the desired background color */
 }
 
 .square {
   width: 40%;
   height: 40%;
-  background-color: #123456; /* Change this value to the desired square color */
+  background-color: ##1d476e; /* Change this value to the desired square color */
   border-radius: 10%; /* Change this value to adjust the roundness of the square edges */
 }
 <Hero />
