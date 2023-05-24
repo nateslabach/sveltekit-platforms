7f97f0e17ad00ff018621755b8b2f0dacf4d8da3
nateslabach <134459912+nateslabach@users.noreply.github.com>
2023-05-24 04:41:45 +0000

    added squares

--- src/lib/apps/hi/components/Home.svelte
+++ src/lib/apps/hi/components/Home.svelte
@@ -1,5 +1,18 @@
 <script>
 	import Hero from './Hero.svelte'
 </script>
+.home.svelte{
+	display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 80vh; /* Adjust this value to change the height of the hero section */
+  background-color: #f0f0f0; /* Change this value to the desired background color */
+}
 
+.square {
+  width: 40%;
+  height: 40%;
+  background-color: #123456; /* Change this value to the desired square color */
+  border-radius: 10%; /* Change this value to adjust the roundness of the square edges */
+}
 <Hero />
--- src/lib/apps/hi/components/structure/Body.svelte
+++ src/lib/apps/hi/components/structure/Body.svelte
@@ -1,1 +1,7 @@
 <slot />
+<div="home.svelte"></div>
+    <div class="square"></div>
+    <div class="square"></div>
+    <div class="square"></div>
+    <div class="square"></div>
+</div>
